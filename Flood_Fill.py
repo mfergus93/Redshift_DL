73a8889f91dcb3376d5a13d981e5082ff501d5ca
mfergus93 <mcferguson9@gmail.com>
2024-04-23 08:15:57 -0500

    Flood Fill Updates

--- Flood_Fill.py
+++ Flood_Fill.py
@@ -15,50 +15,41 @@ import numpy as np
 # folder_path= r'D:\galactic_images_raw'
 folder_path=os.path.dirname(os.path.realpath(__file__))
 os.chdir(folder_path)
-image=cv2.imread('flood_fill_test.png', cv2.IMREAD_GRAYSCALE)
 
 for filename in os.listdir(folder_path):
     if filename.endswith('.jpg') or filename.endswith('.png'):
         
         file_path = os.path.join(folder_path, filename)
-        o_img=cv2.imread(file_path)
+        o_img=cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
         
-        img=np.mean(o_img.copy(),2)
-        img=cv2.GaussianBlur(img,(3,3),0)
+
+        img=cv2.GaussianBlur(o_img.copy(),(3,3),0)
         img=img.astype(np.uint8)
         
-        
-        t_value=26
+        #create a mask where we will never consider values under 6 or is it 6 and under?
+        t_value=60
         _, bin_img=cv2.threshold(img, t_value, 255, cv2.THRESH_BINARY)
+        mask = cv2.bitwise_not(bin_img)
+        mask = cv2.copyMakeBorder(mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
 
         # Define the seed point for flood fill (center of the galaxy)
         seed_point = (img.shape[1] // 2, img.shape[0] // 2)
         
-        # Create a mask for flood fill
-        mask = np.zeros((img.shape[0] + 2, img.shape[1] + 2), np.uint8)
-        
         # Flood fill operation
-        cv2.floodFill(bin_img, None, seed_point, 255, 10, 0)
+        cv2.floodFill(image=img, mask=mask, seedPoint=seed_point, newVal=255, \
+                      loDiff=20, upDiff=2, flags= 8 | ( 125 << 8 ))
         
         # Invert the mask to get the filled region
-        filled_region = cv2.bitwise_not(mask)[1:-1, 1:-1]
-        
+        # filled_region = cv2.bitwise_not(mask)[1:-1, 1:-1]
         # Apply the filled region as a mask on the original image
-        central_galaxy = cv2.bitwise_and(bin_img, bin_img, mask=filled_region)
+        # central_galaxy = cv2.bitwise_and(bin_img, bin_img, mask=filled_region)
         
-        # dot_image=img
-        # dot_radius = 5  # Adjust the size of the dot as needed
-        # dot_color = (0, 0, 255)  # BGR color format (red)
-        
-        # cv2.circle(dot_image, (256, 256), dot_radius, dot_color, -1)
-        # # Display the result
-        
-        # cv2.imshow('Dot', dot_image)
+        # Display the result        
         cv2.imshow('Binary', bin_img)
         cv2.imshow('Original', o_img)
         
-        cv2.imshow('Central Galaxy', central_galaxy)
-        cv2.imshow('filled_region', filled_region)
+        # cv2.imshow('Central Galaxy', central_galaxy)
+        # cv2.imshow('filled_region', filled_region)
         cv2.imshow('img', img)
         cv2.imshow('Mask', mask)
         cv2.waitKey(0)
